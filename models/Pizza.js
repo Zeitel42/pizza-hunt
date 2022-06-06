efdc1926b79fe697005abd3038c862c9dea58baa
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-06-06 18:04:31 -0400

    added get,post,put,delete routes and api call in form handler

--- models/Pizza.js
+++ models/Pizza.js
@@ -17,7 +17,8 @@ const PizzaSchema = new Schema({
   },
   toppings: [],
 });
-
+// create the Pizza model using the PizzaSchema
 const Pizza = model("Pizza", PizzaSchema);
 
+// export the Pizza model
 module.exports = Pizza;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
 const mongoose = require("mongoose");
+
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,13 +12,14 @@ app.use(express.static("public"));
 app.use(require("./routes"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27107/pizza-hunt",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
 
+// Use this to log mongo queries being executed!
 mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
