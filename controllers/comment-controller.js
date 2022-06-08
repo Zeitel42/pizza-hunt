cb5187c6fadc0880d6a80cd27c8868f695dc42c3
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-06-07 20:23:06 -0400

    added comment reply functionality tested

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,12 +1,11 @@
 const { Comment, Pizza } = require("../models");
 
 const commentController = {
-  // add a comment to pizza
+  // add comment to pizza
   addComment({ params, body }, res) {
     console.log(body);
     Comment.create(body)
       .then(({ _id }) => {
-        //   console.log(_id);
         return Pizza.findOneAndUpdate(
           { _id: params.pizzaId },
           { $push: { comments: _id } },
@@ -15,14 +14,32 @@ const commentController = {
       })
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
-          res.status(404).json({ message: "This pizza does not, man" });
+          res.status(404).json({ message: "No pizza found with this id!" });
           return;
         }
         res.json(dbPizzaData);
       })
       .catch((err) => res.json(err));
   },
-  //remove a comment
+
+  // add reply to comment
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // remove comment
   removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
       .then((deletedComment) => {
@@ -44,5 +61,16 @@ const commentController = {
       })
       .catch((err) => res.json(err));
   },
+  // remove reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.json(err));
+  },
 };
+
 module.exports = commentController;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const mongoose = require("mongoose");
-
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,8 +7,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-app.use(require("./routes"));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
@@ -22,4 +19,6 @@ mongoose.connect(
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.use(require("./routes"));
+
+app.listen(PORT, () => console.log(` Connected on localhost:${PORT}`));
